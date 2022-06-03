cd916ec2a71165b150edccffa440cdb7579625ec
Leyenda Nerd <66542754+leyendanerd@users.noreply.github.com>
2022-06-03 17:45:54 -0400

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -19,39 +19,6 @@ def division( num1, num2):
         print("La division de=", num1/num2 )
 
 
-while True:
-    print("""
-    Indique la operacion a realizar: 
-
-    1) Sumar
-    2) Restar
-    3) Multiplicar
-    4) Dividir
-    5) Salir de la calculadora
-    """)
-
-    eleccion= int(input("opcion:"))
-
-    if eleccion == 1: 
-       calculadora()
-       sumar(num1, num2)
-
-    elif eleccion == 2: 
-       calculadora()
-       restar(num1, num2)
-
-    elif eleccion == 3: 
-       calculadora()
-       multiplicar(num1, num2)
-
-    elif eleccion == 4: 
-       calculadora()
-       division(num1, num2)
-
-    elif eleccion == 5: 
-       print("Hasta pronto")
-       break
-
 
 
 
